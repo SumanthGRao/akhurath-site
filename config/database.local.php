db4c08c5d5b598a72797a71038fb724241086c77
SumanthGRao <147526397+SumanthGRao@users.noreply.github.com>
2026-06-16 12:35:02 +0530

    Update database.local.php

--- config/database.local.php
+++ config/database.local.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 /** Hostinger — PHP on the same server as MySQL → use localhost. */
 
-define('AKH_DB_DSN', 'mysql:host=localhost;dbname=u113439427_akhurath;charset=utf8mb4');
+define('AKH_DB_DSN', 'mysql:host=srv2214.hstgr.io;dbname=u113439427_akhurath;charset=utf8mb4');
 define('AKH_DB_USER', 'u113439427_akhurath');
 define('AKH_DB_PASS', 'Z9xGove>$?a');
